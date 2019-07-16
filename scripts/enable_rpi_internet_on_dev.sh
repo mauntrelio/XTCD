fae75ff288435cb73dea1fd19f8ff23a442597b6
Maurizio Manetti <maurizio@imanetti.net>
2019-07-16 13:16:47 +0200

    New Wi-Fi configuration for DESY and eduroam Wi-Fi. Additional USB Wi-Fi adapters drivers install script

--- scripts/enable_rpi_internet_on_dev.sh
+++ scripts/enable_rpi_internet_on_dev.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+IFIN=eno2:0
+IFOUT=eno1
+
 sysctl -w net.ipv4.ip_forward=1
-iptables -A FORWARD -i eno2:0 -o eno1 -j ACCEPT
-iptables -A FORWARD -i eno1 -o eno2:0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
+iptables -A FORWARD -i $IFIN -o $IFOUT -j ACCEPT
+iptables -A FORWARD -i $IFOUT -o $IFIN -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -o $IFOUT -j MASQUERADE
+
+
